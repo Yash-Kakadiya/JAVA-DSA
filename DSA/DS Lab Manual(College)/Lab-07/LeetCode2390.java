1babcb0cd8478bded216cc3c07fbb7827649ce3d
Yash-Kakadiya <136950942+Yash-Kakadiya@users.noreply.github.com>
2024-08-05 08:54:39 +0530

    Create LeetCode2390.java

---
+++ DSA/DS Lab Manual(College)/Lab-07/LeetCode2390.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+public class LeetCode2390 {
+
+    public static String removeStars(String s) {
+        Stack<Character> stk=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){
+                stk.pop();
+            }else{
+                stk.push(s.charAt(i));
+            }
+        }
+        StringBuffer output=new StringBuffer();
+        while(stk.size()>0){
+            output.append(stk.pop());
+        }
+        return output.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String s="Yay*sh*h";
+
+        String ans=removeStars(s);
+
+        System.out.println("Output : "+ans);
+    }
+}
